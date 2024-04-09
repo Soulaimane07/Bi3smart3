68aa8fdfbcc609cdccab45bea3440c551cd669c9
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2024-04-09 03:41:27 +0000

    22

--- front/src/Components/ProductsPanier.js
+++ front/src/Components/ProductsPanier.js
@@ -4,8 +4,6 @@ import { MdOutlineRemove } from "react-icons/md";
 import { PiTrashSimpleThin } from "react-icons/pi";
 import { PiHeartStraightFill } from "react-icons/pi";
 
-
-
 function Product({price,setprice}){
     const [counter,setcounter]=useState(1)
    
@@ -15,45 +13,44 @@ function Product({price,setprice}){
         setChecked(!checked);
         console.log(checked)
         checked ? setprice(price+10*counter) : setprice(price-10*counter)
-        
-    };
+    };
 
    return (
-    <div className=' bg-white mb-4 px-2 py-2 flex rounded-md '>
-        <input  value={checked}  onChange={handleChange} type='checkbox' className=' size-6 mr-4 '></input>
-        <img  className='w-40 h-40 ' src='../images/header.jpg' />
-        <div className=' mx-3 w-full flex flex-col'>
-            <div className='  h-full'>   
-                <h1 className='text-2xl'>titre</h1>
-                <h1 className=' text-xl font-bold'>3.45$</h1>
-            </div>
-        
-            <div className=' flex space-x-2 justify-end  items-center'>
-                <div className='flex'>
-                    <button onClick={()=>setcounter(counter+1)} className=' border-2 rounded-l-lg px-2'>
-                        <TiPlus />
-                    </button>
-                    <h1 className=' border-t-2 border-b-2 px-2'>{counter}</h1>
-                    <button disabled={counter==1} onClick={()=>setcounter(counter-1)  } className={` border-2 rounded-r-lg px-2 ${counter==1 && 'opacity-40'}`}>
-                        <MdOutlineRemove />
-                    </button>
+        <div className=' bg-white mb-4 px-2 py-2 flex rounded-md '>
+            <input  value={checked}  onChange={handleChange} type='checkbox' className=' size-6 mr-4 '></input>
+            <img  className='w-40 h-40 ' alt='' src='../images/header.jpg' />
+            <div className=' mx-3 w-full flex flex-col'>
+                <div className='  h-full'>   
+                    <h1 className='text-2xl'>titre</h1>
+                    <h1 className=' text-xl font-bold'>3.45$</h1>
                 </div>
-                <button className=' opacity-40 hover:opacity-100 transition-all '><PiTrashSimpleThin size={25} /></button>
-                <button className='opacity-20 hover:text-red-600 hover:opacity-100  transition-all'><PiHeartStraightFill size={25}/></button>
-            </div> 
+            
+                <div className=' flex space-x-2 justify-end  items-center'>
+                    <div className='flex'>
+                        <button onClick={()=>setcounter(counter+1)} className=' border-2 rounded-l-lg px-2'>
+                            <TiPlus />
+                        </button>
+                        <h1 className=' border-t-2 border-b-2 px-2'>{counter}</h1>
+                        <button disabled={counter===1} onClick={()=>setcounter(counter-1)  } className={` border-2 rounded-r-lg px-2 ${counter===1 && 'opacity-40'}`}>
+                            <MdOutlineRemove />
+                        </button>
+                    </div>
+                    <button className=' opacity-40 hover:opacity-100 transition-all '><PiTrashSimpleThin size={25} /></button>
+                    <button className='opacity-20 hover:text-red-600 hover:opacity-100  transition-all'><PiHeartStraightFill size={25}/></button>
+                </div> 
+
+            </div>
 
         </div>
-
-    </div>
    )
 }
+
 function ProductsPanier({products,price,setprice}) {
   return (
     <div className=' bg-gray-100 px-4  py-4  flex-col min-h-screen rounded-md'>
         <div>
             {products.map((item,key)=>(
                 <Product price={price} setprice={setprice}/>
-
             ))}
            
         </div>
--- front/src/Pages/Panier/Panier.js
+++ front/src/Pages/Panier/Panier.js
@@ -5,8 +5,6 @@ import Footer from '../../Components/Footer/Footer';
 import { Link } from 'react-router-dom';
 import ProductsPanier from '../../Components/ProductsPanier';
 
-
-
 function Panier() {
     let image =[
        '../images/visa.svg',
@@ -28,17 +26,17 @@ function Panier() {
         <main className='min-h-screen mt-28 py-8 flex mx-10 space-x-6'>
             <div className='flex-1'>
                 <h1 className=' text-2xl mb-4 bg-gray-100 px-10  py-6 rounded-md font-bold '> ALL ITEMS ({products.length})</h1>
-                {products.length==0 ? 
-                <div className=' bg-gray-100 px-16  py-10 flex justify-center flex-col min-h-screen rounded-md'>
-                    <div className=' mx-auto opacity-70 mb-10'> <BsCartDash size={100}/></div>
-                    <h1 className='text-xl text-center mb-1 font-bold'>YOUR CART IS EMPTY</h1>
-                    <p className='text-center opacity-80'> Connectez-vous pour voir votre panier et commencer vos achats. </p>
-                    <Link to='/Products' className='mt-6 w-fit mx-auto text-lg bg-black text-white rounded-md px-20 py-3 hover:scale-105 transition-all'>SHOP NOW</Link>
-                   
-                </div>
-                :<ProductsPanier products={products} price={price} setprice={setprice}/>}
-                
-               
+                {products.length === 0 
+                    ? 
+                        <div className=' bg-gray-100 px-16  py-10 flex justify-center flex-col min-h-screen rounded-md'>
+                            <div className=' mx-auto opacity-70 mb-10'> <BsCartDash size={100}/></div>
+                            <h1 className='text-xl text-center mb-1 font-bold'>YOUR CART IS EMPTY</h1>
+                            <p className='text-center opacity-80'> Connectez-vous pour voir votre panier et commencer vos achats. </p>
+                            <Link to='/Products' className='mt-6 w-fit mx-auto text-lg bg-black text-white rounded-md px-20 py-3 hover:scale-105 transition-all'>SHOP NOW</Link>
+                        </div>
+                    :
+                        <ProductsPanier products={products} price={price} setprice={setprice}/>
+                }
             </div>
 
             <div className='w-96 h-60 right-0'>
@@ -51,7 +49,7 @@ function Panier() {
                     <h1 className='mb-6 font-bold text-lg'>We Accept</h1>
                     <div className=' grid grid-cols-6 items-center '>
                         {image.map((item,key)=>(
-                            <img key={key} src={item} className='w-10' />
+                            <img key={key} src={item} alt='' className='w-10' />
                         ))}
                     </div>
                 </div>
