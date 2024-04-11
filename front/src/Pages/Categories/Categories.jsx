7773a74f43ffb5b60a64529265e3188cbd9c1269
lastpi1ot <hamzaett1320@gmail.com>
2024-04-11 17:05:57 +0000

    footer

--- front/src/Pages/Categories/Categories.jsx
+++ front/src/Pages/Categories/Categories.jsx
@@ -10,7 +10,7 @@ export const Categories = () => {
     <Navbar />
     <main className='min-h-screen mt-28 flex items-start'>
         <Filtrage />
-        <div className='w-5/6'>
+        <div className='w-5/6 border-l-2 '>
           <h1 className=' text-2xl mb-4 bg-gray-100 px-10  py-6 rounded-md font-bold text-center '> Categorie1</h1>
           <div className=' grid grid-cols-4 gap-6 px-20 py-8'>
           {list.map(()=><Product/>)}
--- front/src/Pages/Favorits/Favorits.jsx
+++ front/src/Pages/Favorits/Favorits.jsx
@@ -10,7 +10,7 @@ function Favorits(){
         <Navbar/>
         <main className='min-h-screen mt-28 flex'>
              <Filtrage/>
-             <div className="w-full mt-10 p-10 grid grid-cols-4 gap-8" >
+             <div className="w-full  p-10 grid grid-cols-4 gap-8 border-l-2" >
                 {list.map(()=>(
                     <Product/> 
                 ))}
