39ee14088cb232d5903aadab439d7c3f9a8f28af
lastpi1ot <hamzaett1320@gmail.com>
2024-04-11 17:08:10 +0000

    Update Categories.jsx

--- front/src/Pages/Categories/Categories.jsx
+++ front/src/Pages/Categories/Categories.jsx
@@ -11,7 +11,7 @@ export const Categories = () => {
     <main className='min-h-screen mt-28 flex items-start'>
         <Filtrage />
         <div className='w-5/6 border-l-2 '>
-          <h1 className=' text-2xl mb-4 bg-gray-100 px-10  py-6 rounded-md font-bold text-center '> Categorie1</h1>
+          <h1 className=' text-2xl mb-4 px-10 mt-4 py-6 rounded-md   '> / Categorie1</h1>
           <div className=' grid grid-cols-4 gap-6 px-20 py-8'>
           {list.map(()=><Product/>)}
           </div>
