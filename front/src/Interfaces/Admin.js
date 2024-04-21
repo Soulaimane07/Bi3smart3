beb585ef1a3c0955953c1186e3e3bd64c753327b
fatimaezzahrakld <ezzahra366@gmail.com>
2024-04-21 20:54:44 +0100

    admin

--- front/src/Interfaces/Admin.js
+++ front/src/Interfaces/Admin.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ReadUser from '../Pages/Admin/User/ReadUser'
+
 import AddUser from '../Pages/Admin/User/AddUser'
 import EditUser from '../Pages/Admin/User/EditUser'
 import Dashboard from '../Pages/Admin/Dashboard/Dashboard'
@@ -9,12 +10,19 @@ function Admin() {
   return (
     
     <>
-    <Routes>
-      <Route path="/admin/dashboard" element={<Dashboard />} />
+    <Route path="/admin/dashboard" element={<Dashboard />} />
         <Route path="/admin/readuser" element={<ReadUser />} />
         <Route path="/admin/adduser" element={<AddUser />} />
         <Route path="/admin/edituser/:id" element={<EditUser />} />
-    </Routes>
+        <Route path="/admin/readcategorie" element={<ReadCategorie />} />
+        <Route path="/admin/addcategorie" element={<AddCategorie />} />
+        <Route path="/admin/editcategorie/:id" element={<EditCategorie />} />
+        <Route path="/admin/readProduct" element={<ReadProduct />} />
+        <Route path="/admin/addProduct" element={<AddProduct />} />
+        <Route path="/admin/editProduct/:id" element={<EditProduct />} />
+        <Route path="/admin/readSeller" element={<ReadSeller />} />
+        <Route path="/admin/addSeller" element={<AddSeller />} />
+        <Route path="/admin/editSeller/:id" element={<EditSeller />} />
 </>
   )
 }
