61ec58c0a5b9a406abaada26be2d135ba0823707
lastpi1ot <hamzaett1320@gmail.com>
2024-04-30 15:16:48 +0100

    dashboard

--- front/src/Pages/Admin/Dashboard/Dashboard.jsx
+++ front/src/Pages/Admin/Dashboard/Dashboard.jsx
@@ -3,20 +3,32 @@ import Sidebar from '../../../Components/Navbar/Sidebar'
 import { Link } from 'react-router-dom'
 import Footer from '../../../Components/Footer/Footer'
 import SellerNavbar from '../../../Components/Navbar/Seller/SellerNavbar'
-
+import { GetUsers,GetProducts,GetCategories } from '../../../Components/Functions';
 function Dashboard() {
+    const users = GetUsers()
+    const products = GetProducts()
+    const categories = GetCategories()
     const list =[
+        {
+            image: "../images/user.png",
+            title: "Users",
+            stats:users?.length
+           
+        },
         {
             image: "../images/products.png",
             title: "Products",
-            stats: "",
+            stats: products?.length,
            
         },
         {
             image: "../images/categories.png",
             title: "Catrgories",
+            stats: categories?.length,
+           
            
         },
+        
     ]
     
   return (
@@ -26,12 +38,13 @@ function Dashboard() {
     
     <main className='min-h-screen mt-28 flex items-start'>
     <Sidebar />
-    <div className='flex flex-row mt-6 space-x-4'>
+    <div className='flex w-4/5  ml-10 mt-6 space-x-4'>
         {list.map((item,key)=>(
-            <div key={key} className="w-full  p-6 bg-white border border-gray-200 rounded-lg shadow">
-                <Link to={item.link} className='flex items-center space-x-2'>
+            <div key={key} className="w-full h-34  p-6 bg-white border border-gray-200 rounded-lg shadow">
+                <Link to={item.link} className='flex items-center w-68 space-x-2'>
                     <img src={item.image} alt='logo' className='w-10' />
                     <h5 className=" text-2xl font-medium tracking-tight text-gray-900"> {item.title} </h5>
+                    <h5 className=" text-2xl font-medium tracking-tight text-gray-900"> ( {item.stats} ) </h5>
                 </Link>
                 
             </div>
